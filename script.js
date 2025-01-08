429866a292a892fbd62ec77d7bb9be89d1020066
Sabin Ghimire <sabinghimire223@gmail.com>
2025-01-08 14:58:43 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const var1 = document.querySelector(".btn");
+const var2 = document.querySelector("#city");
+const temp = document.querySelector("#temp-div");
+const api = "7a3ffbf448ad87f45f949a7efe9e44dc";
+
+var1.addEventListener("click", async function () {
+  let city = var2.value;
+  const weatherinfo = await getweatherinfo(city);
+  const data = weatherinfo.main.temp + "°C";
+  temp.innerHTML = data;
+});
+
+async function getweatherinfo(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
